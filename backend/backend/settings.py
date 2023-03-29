f0f5592cedd0a024cd7e5f1b4292c15365f110a9
s-goyal23 <goyal.23@iitj.ac.in>
2023-03-29 12:21:56 +0530

    Team API functional
    Made minor changes in backend/team/views.py to make team API functional.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l*))a-0+r9*-2qo&87@h%x&91e!3-o8x(uiuex$7r7hw_l&vjz'
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
