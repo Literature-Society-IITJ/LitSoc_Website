641dd75542ff5411da129fd4ca5ced11dd71712f
s-goyal23 <goyal.23@iitj.ac.in>
2023-04-08 13:11:52 +0530

    fixed the error in TeamView
    fixed the error in TeamView in views,py so that it correctly displays team details

--- backend/team/views.py
+++ backend/team/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from team.serializers import TeamViewSerializer, TeamUpdateSerializer
 from django.contrib.admin.views.decorators import staff_member_required
+from team.models import Team
 # Create your views here.
 
 class TeamUpdate(APIView):
@@ -15,7 +16,12 @@ class TeamUpdate(APIView):
         return Response(serializer.errors)
 
 class TeamView(APIView):
+    queryset = Team.objects.all()
+    serializer_class = TeamViewSerializer()
     def get(self, request, format=None):
-        serializer = TeamViewSerializer()
-        return Response(serializer.data)
+        query_set = list(self.queryset.values())
+        return Response(query_set)
+        # if serializer.is_valid(raise_exception=True):
+        #     return Response(serializer.data)
+        # return Response(serializer.errors)
     
