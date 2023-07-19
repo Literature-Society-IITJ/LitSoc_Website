10c41b8797d94c1122a50f11b36763ad267426a1
Literature Society IITJ <137613584+LitSoc-IITJ@users.noreply.github.com>
2023-07-19 12:32:14 +0530

    Update Clubs.js

--- frontend/src/data/Clubs.js
+++ frontend/src/data/Clubs.js
@@ -44,7 +44,7 @@ export const ClubsData = {
         clubname: "GOONJ",
         imgsrc: 'src/media/images/goonjlogo.jpg',
 
-        description: "Welcome to the college book club! We are a group of students who share a love for reading and a desire to engage with literature in a deeper, more meaningful way. Our club is open to anyone who wants to join, regardless of their major, background, or level of experience with books. Each semester, we will select a series of books to read and discuss, ranging from classics of literature to contemporary bestsellers. Our goal is to foster a community of readers who can share their thoughts, insights, and perspectives on a wide range of texts. Whether you're an avid reader or just starting to explore the world of books, we invite you to join us on this exciting journey of discovery and learning.",
+        description: "Goonj is a club of Literature Society for the people who are more interested in languages ​​other than English literature. People with similar interests meet and enjoy their passion to the utmost through goonj's different frequent online events and offline meetups. Also, regular competitions are conducted to encourage the writers to hold their pens and compile their world of thoughts!",
 
         quote: "\"There is no friend as loyal as a book.\" - Ernest Hemingway.",
     },
