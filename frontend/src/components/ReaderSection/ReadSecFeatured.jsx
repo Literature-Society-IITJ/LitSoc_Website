47ef92a1bdcbf80785404a8ebb5a90508986b6ce
Shubh Goyal <goyal.shubh0711@gmail.com>
2023-07-21 17:55:50 +0530

    Reader Section completely responsive and library issue resolved

--- frontend/src/components/ReaderSection/ReadSecFeatured.jsx
+++ frontend/src/components/ReaderSection/ReadSecFeatured.jsx
@@ -6,7 +6,7 @@ export default function ReadSecFeatured() {
         <div className='reader-sec featured-image'>
         </div>
         <div className='reader-sec featured-content'>
-            <h1 style={{fontSize: `80px`, fontWeight: `bold`, fontFamily: `cursive`}}>Syahi Darpan</h1>
+            <h1 className='reader-sec-featured-title'>Syahi Darpan</h1>
             'Syahi Darpan' is a captivating anthology showcasing the literary brilliance of the IITJ community. This book brims with thought-provoking poems, poignant stories, and insightful essays, reflecting the creative spirit and intellectual depth of its contributors over the past few years.
         </div>
     </div>
