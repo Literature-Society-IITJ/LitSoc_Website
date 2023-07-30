2c7c9e1de2ad6b9847872aba25a2688ccd589b3d
Literature Society IITJ <137613584+LitSoc-IITJ@users.noreply.github.com>
2023-07-31 00:03:29 +0530

    Update HomeMiddle.jsx

--- frontend/src/components/HomePage/HomeMiddle.jsx
+++ frontend/src/components/HomePage/HomeMiddle.jsx
@@ -8,7 +8,7 @@ import HomeStarfield from './HomeStarfield'
 export default function Home() {
     return (
         <div className='home-main-inner'>
-            <HomeStarfield />
+{/*             <HomeStarfield /> */}
             <HomeFeatured />
             <HomeSocDesc />
             <HomeAllSecDesc />
