0c77c4b2d89ebd6f8ff2a0e908f9c1e581f95c07
Shubh Goyal <goyal.shubh0711@gmail.com>
2023-08-06 00:55:14 +0530

    MInor changes and animations

--- frontend/src/components/HomePage/HomeAllSecDesc.jsx
+++ frontend/src/components/HomePage/HomeAllSecDesc.jsx
@@ -1,6 +1,41 @@
 import React from 'react'
 import { HomeAllSecDescList } from '../../data/HomePageData'
 
+
+
+const addDescriptionAnimation = () => {
+    const homeAllSecDescCard = document.querySelectorAll(".home-all-desc-card");
+    homeAllSecDescCard.forEach(elem => {
+        if (elem.getBoundingClientRect().top < window.innerHeight-60) {
+            // elem.classList.add('home-all-desc-card-animate');
+            // document.querySelectorAll(".home-all-desc-card-border")[0].classList.add('home-all-desc-card-border-animate');
+            elem.style.animation = 'HomeAllDescFadeIn 1s forwards ease-in-out 0s';
+        }
+    })
+
+    const homeAllSecDescCardBorder = document.querySelectorAll(".home-all-desc-card-border");
+    homeAllSecDescCardBorder.forEach(elem => {
+        if (elem.getBoundingClientRect().top < window.innerHeight-60) {
+            // elem.classList.add('home-all-desc-card-border-animate');
+            // document.querySelectorAll(".home-all-desc-card-border")[0].classList.add('home-all-desc-card-border-animate');
+            elem.style.animation = 'HomeAllDescBorderScale 1s forwards ease-in-out 0s';
+        }
+    })
+
+    const homeAllSecDescCard45DegContainer = document.querySelectorAll(".home-all-desc-45rotate-container");
+    homeAllSecDescCard45DegContainer.forEach(elem => {
+        if (elem.getBoundingClientRect().top < window.innerHeight-60) {
+            // elem.classList.add('home-all-desc-45rotate-container-animate');
+            // document.querySelectorAll(".home-all-desc-card-border")[0].classList.add('home-all-desc-card-border-animate');
+            // console.log(elem.style)
+            elem.style.animation = 'HomeAllSec45DegRotate 1s forwards ease-in-out 0s';
+        }
+    })
+};
+
+window.addEventListener('scroll', addDescriptionAnimation);
+
+
 export default function HomeAllSecDesc() {
     return (
         <div className='home-all-sections-desc'>
