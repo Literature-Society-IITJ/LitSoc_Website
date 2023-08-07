5d8bd8eb70d45595a7dfe8a1f75599671ad937a5
Ubuntu <ubuntu@ip-172-31-42-66.ap-south-1.compute.internal>
2023-08-07 10:19:51 +0000

    Production Updates Database

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 # from home.views import MemberViewSet
 from rest_framework.routers import DefaultRouter
-
+from backend.settings import ADMIN_ENABLED
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,6 +31,9 @@ urlpatterns = [
     # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
+#if ADMIN_ENABLED:
+#   urlpatterns += path('admin/', admin.site.urls)
 
 # router = DefaultRouter()
 # router.register('user', MemberViewSet, basename='user')
