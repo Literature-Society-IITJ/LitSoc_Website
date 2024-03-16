1b48ce634a85e1833a1990699a2d1f16795d697e
Shubh Goyal <goyal.shubh0711@gmail.com>
2024-03-17 01:57:14 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -225,14 +225,14 @@ export async function getProfileImage() {
     let baseURL = url
     let { access_token } = getToken()
 
-    // console.log(222222222)
+    console.log(222222222)
 
     let response = await axios.get(`${baseURL}home/updateprofileimage/`,
                                     {headers: {'Authorization': `Bearer ${access_token}`, 
                                     'Content-Type': 'application/json'}})
 
-    // console.log(33333333333333)
-    // console.log(response.data)
+    console.log(33333333333333)
+    console.log(response.data)
     return response.data
 }
 
@@ -242,18 +242,18 @@ export async function updateProfileImage(formData) {
 
     // console.log(1111111111)
     // console.log(formData.get("myImage"))
-    formData.append('access_token', access_token)
+    // formData.append('access_token', access_token)
     // console.log(formData.get("access_token"))
-    // console.log(formData)
-    // console.log(1)
+    console.log(1)
+    console.log(formData)
 
     let response = await axios.post(`${baseURL}home/updateprofileimage/`, 
                                     formData,
                                     { headers: {'Authorization': `Bearer ${access_token}`, 
                                     'Content-Type': 'multipart/form-data'} })
 
-    // console.log(2)
-    // console.log(response.data)
+    console.log(2)
+    console.log(response.data)
 
     return response.data
 }
